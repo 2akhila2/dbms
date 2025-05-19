ab03c00c51e1d9f116f0ed9279d4201884dbcef7
Akhila <akhilaammu582@gmail.com>
2025-05-19 15:14:57 +0530

    Add files via upload

---
+++ akhilatrigger.sql
@@ -0,0 +1,11 @@
+create or replace trigger t1 before insert or update or delete on customer for each row
+when(new.CUST_ID>0)
+declare
+ sal_diff number;
+ begin
+ sal_diff :=:new.salary-:old.salary;
+ dbms_output.put_line('old salary '||:old.salary);
+ dbms_output.put_line('new salary '||:new.salary);
+ dbms_output.put_line(' salary difference'||sal_diff);
+ end;
+ /
